d085dc0c5d5012cc48e3c51c6620226fc0a75ee6
vinson <vinson2217@gmail.com>
2025-10-31 14:47:42 +0530

    updated code

---
+++ frount-end/src/components/hero/hero.jsx
@@ -0,0 +1,14 @@
+"use client"
+
+import React from "react";
+
+const Hero = () => {
+    return (
+        <>
+        <h1>Hellow vanakkam Guys</h1>
+        <h1>u111</h1>
+        </>
+    )
+}
+
+export default Hero;
