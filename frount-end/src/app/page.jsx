89985a42f1e6a39616236fba12abc51057190609
vinson <vinson2217@gmail.com>
2025-10-31 14:39:31 +0530

    updates from vinson git lesson

--- frount-end/src/app/page.jsx
+++ frount-end/src/app/page.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Hero from "@/components/hero/hero"
 
 export default function Home() {
   return (
     <div className="">
-      
+      <Hero />
     </div>
   );
 }
---
+++ frount-end/src/components/hero/hero.jsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from "react";
+
+const Hero = () => {
+    return (
+        <>
+        <h1>Hellow vanakkam Guys</h1>
+        </>
+    )
+}
+
+export default Hero;
